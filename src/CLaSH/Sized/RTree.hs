bb02cdec6edd11c696429abac85f4175ada94349
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-02-01 14:59:10 +0100

    Extend RTree module

--- src/CLaSH/Sized/RTree.hs
+++ src/CLaSH/Sized/RTree.hs
@@ -5,7 +5,8 @@ Maintainer :  Christiaan Baaij <christiaan.baaij@gmail.com>
 -}
 
 {-# LANGUAGE DataKinds, TypeOperators, GADTs, ScopedTypeVariables,
-             KindSignatures, RankNTypes #-}
+             KindSignatures, RankNTypes, TypeFamilies, UndecidableInstances,
+             MagicHash, PatternSynonyms, FlexibleContexts #-}
 
 {-# LANGUAGE Trustworthy #-}
 
@@ -13,16 +14,24 @@ Maintainer :  Christiaan Baaij <christiaan.baaij@gmail.com>
 
 module CLaSH.Sized.RTree where
 
-import Data.Singletons.Prelude     (TyFun,type ($))
+import Data.Singletons.Prelude     (Apply, TyFun, type ($))
 import Data.Proxy                  (Proxy (..))
-import GHC.TypeLits                (KnownNat, Nat, type (+))
+import GHC.TypeLits                (KnownNat, Nat, type (+), type (^), type (*))
+import qualified Prelude           as P
+import Prelude                     hiding ((++), (!!))
 
-import CLaSH.Promoted.Nat          (SNat (..), subSNat)
-import CLaSH.Promoted.Nat.Literals (d1)
+import CLaSH.Class.BitPack         (BitPack (..))
+import CLaSH.Promoted.Nat          (SNat (..), UNat (..), subSNat, powSNat, toUNat, snatToInteger)
+import CLaSH.Promoted.Nat.Literals (d1, d2)
+import CLaSH.Sized.Vector          (Vec (..), pattern (:>), (++), (!!))
 
 data RTree :: Nat -> * -> * where
   LR :: a -> RTree 0 a
   BR :: RTree n a -> RTree n a -> RTree (n+1) a
+
+instance Show a => Show (RTree n a) where
+  show (LR a)   = show a
+  show (BR l r) = '<':show l P.++ (',':show r) P.++ ">"
 
 tfold :: forall p k a . KnownNat k
       => Proxy (p :: TyFun Nat * -> *)
@@ -36,3 +45,37 @@ tfold _ f g = go SNat
     go _  (LR a)   = f a
     go sn (BR l r) = let sn' = sn `subSNat` d1
                      in  g sn' (go sn' l) (go sn' r)
+{-# NOINLINE tfold #-}
+
+treplicate :: forall d a . SNat d -> a -> RTree d a
+treplicate sn a = go (toUNat sn)
+  where
+    go :: UNat n -> RTree n a
+    go UZero      = LR a
+    go (USucc un) = BR (go un) (go un)
+{-# NOINLINE treplicate #-}
+
+data CTree (a :: *) (f :: TyFun Nat *) :: *
+type instance Apply (CTree a) d = RTree d a
+
+tmap :: KnownNat d => (a -> b) -> RTree d a -> RTree d b
+tmap f = tfold (Proxy :: Proxy (CTree d)) (LR . f) (\_ l r -> BR l r)
+
+data VTree (a :: *) (f :: TyFun Nat *) :: *
+type instance Apply (VTree a) d = Vec (2^d) a
+
+t2v :: KnownNat d => RTree d a -> Vec (2^d) a
+t2v = tfold (Proxy :: Proxy (VTree a)) (:> Nil) (\_ l r -> l ++ r)
+
+tindices :: KnownNat d => RTree d Integer
+tindices = tfold (Proxy :: Proxy (CTree Integer)) LR
+                 (\s@SNat l r-> BR l (tmap (+(snatToInteger (d2 `powSNat` s))) r))
+                 (treplicate SNat 0)
+
+v2t :: (KnownNat d, KnownNat (2^d)) => Vec (2^d) a -> RTree d a
+v2t v = tmap (v !!) tindices
+
+instance (KnownNat (2^d), KnownNat d, KnownNat (BitSize a), BitPack a) => BitPack (RTree d a) where
+  type BitSize (RTree d a) = (2^d) * (BitSize a)
+  pack   = pack . t2v
+  unpack = v2t . unpack
