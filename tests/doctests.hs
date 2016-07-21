68bbbbeb3fffaa3653177f9f56dd921ff006b306
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-07-21 12:30:59 +0200

    Fix doctest so that CPP finds the include dir

--- tests/doctests.hs
+++ tests/doctests.hs
@@ -3,4 +3,4 @@ module Main where
 import Test.DocTest (doctest)
 
 main :: IO ()
-main = doctest ["-i src","CLaSH.Prelude","CLaSH.Examples","CLaSH.Tutorial"]
+main = doctest ["-isrc","-optP-I","-optPinclude","CLaSH.Prelude","CLaSH.Examples","CLaSH.Tutorial"]
