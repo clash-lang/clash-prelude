162bb54184e5d2f7d72c0f50453f3e5dd8f47a9c
Christiaan Baaij <christiaan.baaij@gmail.com>
2016-07-28 17:27:07 +0200

    Adapt ROM to new clock/reset API

--- src/CLaSH/Prelude/ROM.hs
+++ src/CLaSH/Prelude/ROM.hs
@@ -6,9 +6,10 @@ Maintainer :  Christiaan Baaij <christiaan.baaij@gmail.com>
 ROMs
 -}
 
-{-# LANGUAGE DataKinds     #-}
-{-# LANGUAGE MagicHash     #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE ImplicitParams   #-}
+{-# LANGUAGE MagicHash        #-}
+{-# LANGUAGE TypeOperators    #-}
 
 {-# LANGUAGE Safe #-}
 
@@ -23,21 +24,21 @@ module CLaSH.Prelude.ROM
   , rom
   , romPow2
     -- * Synchronous ROM synchronised to an arbitrary clock
-  , rom'
-  , romPow2'
+  , rom#
+  , romPow2#
     -- * Internal
   , asyncRom#
-  , rom#
+  , rom##
   )
 where
 
 import Data.Array             ((!),listArray)
+import GHC.Stack              (HasCallStack, withFrozenCallStack)
 import GHC.TypeLits           (KnownNat, type (^))
 
-import CLaSH.Signal           (Signal)
-import CLaSH.Signal.Explicit  (Signal', SClock, systemClock)
+import CLaSH.Signal           (Signal, Clock, ClockKind (..))
 import CLaSH.Sized.Unsigned   (Unsigned)
-import CLaSH.Signal.Explicit  (register')
+import CLaSH.Signal.Explicit  (delay#)
 import CLaSH.Sized.Vector     (Vec, maxIndex, toList)
 
 {-# INLINE asyncRom #-}
@@ -93,13 +94,13 @@ asyncRom# content rd = arr ! rd
 --
 -- * See "CLaSH.Sized.Fixed#creatingdatafiles" and "CLaSH.Prelude.BlockRam#usingrams"
 -- for ideas on how to use ROMs and RAMs
-rom :: (KnownNat n, KnownNat m)
+rom :: (HasCallStack, KnownNat n, KnownNat m, ?clk :: Clock 'Original domain)
     => Vec n a               -- ^ ROM content
                              --
                              -- __NB:__ must be a constant
-    -> Signal (Unsigned m)   -- ^ Read address @rd@
-    -> Signal a              -- ^ The value of the ROM at address @rd@
-rom = rom' systemClock
+    -> Signal domain (Unsigned m)   -- ^ Read address @rd@
+    -> Signal domain a              -- ^ The value of the ROM at address @rd@
+rom = rom# ?clk
 
 {-# INLINE romPow2 #-}
 -- | A ROM with a synchronous read port, with space for 2^@n@ elements
@@ -111,15 +112,15 @@ rom = rom' systemClock
 --
 -- * See "CLaSH.Sized.Fixed#creatingdatafiles" and "CLaSH.Prelude.BlockRam#usingrams"
 -- for ideas on how to use ROMs and RAMs
-romPow2 :: KnownNat n
+romPow2 :: (HasCallStack, KnownNat n, ?clk :: Clock 'Original domain)
         => Vec (2^n) a         -- ^ ROM content
                                --
                                -- __NB:__ must be a constant
-        -> Signal (Unsigned n) -- ^ Read address @rd@
-        -> Signal a            -- ^ The value of the ROM at address @rd@
-romPow2 = romPow2' systemClock
+        -> Signal domain (Unsigned n) -- ^ Read address @rd@
+        -> Signal domain a            -- ^ The value of the ROM at address @rd@
+romPow2 = rom# ?clk
 
-{-# INLINE romPow2' #-}
+{-# INLINE romPow2# #-}
 -- | A ROM with a synchronous read port, with space for 2^@n@ elements
 --
 -- * __NB__: Read value is delayed by 1 cycle
@@ -129,16 +130,16 @@ romPow2 = romPow2' systemClock
 --
 -- * See "CLaSH.Sized.Fixed#creatingdatafiles" and "CLaSH.Prelude.BlockRam#usingrams"
 -- for ideas on how to use ROMs and RAMs
-romPow2' :: KnownNat n
-         => SClock clk               -- ^ 'Clock' to synchronize to
+romPow2# :: (HasCallStack, KnownNat n)
+         => Clock clk domain               -- ^ 'Clock' to synchronize to
          -> Vec (2^n) a              -- ^ ROM content
                                      --
                                      -- __NB:__ must be a constant
-         -> Signal' clk (Unsigned n) -- ^ Read address @rd@
-         -> Signal' clk a            -- ^ The value of the ROM at address @rd@
-romPow2' = rom'
+         -> Signal domain (Unsigned n) -- ^ Read address @rd@
+         -> Signal domain a            -- ^ The value of the ROM at address @rd@
+romPow2# = rom#
 
-{-# INLINE rom' #-}
+{-# INLINE rom# #-}
 -- | A ROM with a synchronous read port, with space for @n@ elements
 --
 -- * __NB__: Read value is delayed by 1 cycle
@@ -148,27 +149,27 @@ romPow2' = rom'
 --
 -- * See "CLaSH.Sized.Fixed#creatingdatafiles" and "CLaSH.Prelude.BlockRam#usingrams"
 -- for ideas on how to use ROMs and RAMs
-rom' :: (KnownNat n, Enum addr)
-     => SClock clk       -- ^ 'Clock' to synchronize to
+rom# :: (HasCallStack, KnownNat n, Enum addr)
+     => Clock clk domain -- ^ 'Clock' to synchronize to
      -> Vec n a          -- ^ ROM content
                          --
                          -- __NB:__ must be a constant
-     -> Signal' clk addr -- ^ Read address @rd@
-     -> Signal' clk a
+     -> Signal domain addr -- ^ Read address @rd@
+     -> Signal domain a
      -- ^ The value of the ROM at address @rd@ from the previous clock cycle
-rom' clk content rd = rom# clk content (fromEnum <$> rd)
+rom# clk content rd = (withFrozenCallStack rom#) clk content (fromEnum <$> rd)
 
-{-# NOINLINE rom# #-}
+{-# NOINLINE rom## #-}
 -- | ROM primitive
-rom# :: KnownNat n
-     => SClock clk      -- ^ 'Clock' to synchronize to
-     -> Vec n a         -- ^ ROM content
-                        --
-                        -- __NB:__ must be a constant
-     -> Signal' clk Int -- ^ Read address @rd@
-     -> Signal' clk a
-     -- ^ The value of the ROM at address @rd@ from the previous clock cycle
-rom# clk content rd = register' clk undefined ((arr !) <$> rd)
+rom## :: (HasCallStack, KnownNat n)
+      => Clock clk domain -- ^ 'Clock' to synchronize to
+      -> Vec n a          -- ^ ROM content
+                          --
+                          -- __NB:__ must be a constant
+      -> Signal domain Int  -- ^ Read address @rd@
+      -> Signal domain a
+      -- ^ The value of the ROM at address @rd@ from the previous clock cycle
+rom## clk content rd = (withFrozenCallStack delay#) clk ((arr !) <$> rd)
   where
     szI = maxIndex content
     arr = listArray (0,szI) (toList content)
